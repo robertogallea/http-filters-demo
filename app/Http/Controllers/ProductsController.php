817f58e7b083b9bb165cc4dcca94705992cddf7a
roberto.gallea <write@robertogallea.com>
2020-04-01 09:22:15 +0200

    Use scopes

--- app/Http/Controllers/ProductsController.php
+++ app/Http/Controllers/ProductsController.php
@@ -10,12 +10,8 @@ class ProductsController extends Controller
     public function index(Request $request)
     {
         $products = Product::query()
-            ->when($request->search, function ($q) use ($request) {
-                $q->where('name', 'like', "%{$request->search}%");
-            })
-            ->when($request->sort, function ($q) use ($request) {
-                $q->orderBy($request->sort, $request->dir === 'desc' ? 'desc' : 'asc');
-            })
+            ->search($request->search)
+            ->sort($request->sort, $request->dir)
             ->get();
 
         return response()->json($products);
--- app/Product.php
+++ app/Product.php
@@ -2,9 +2,22 @@
 
 namespace App;
 
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
-    //
+    public function scopeSearch(Builder $query, ?string $search)
+    {
+        $query->when($search, function ($q) use ($search) {
+            $q->where('name', 'like', "%{$search}%");
+        });
+    }
+
+    public function scopeSort(Builder $query, ?string $sort, ?string $dir)
+    {
+        $query->when($sort, function ($q) use ($sort, $dir) {
+            $q->orderBy($sort, $dir === 'desc' ? 'desc' : 'asc');
+        });
+    }
 }
