6139072cc4a7fb51441bd0a92ebe2d22218cafbe
roberto.gallea <write@robertogallea.com>
2020-04-01 10:31:22 +0200

    Refactor models with trait

--- app/Product.php
+++ app/Product.php
@@ -2,16 +2,9 @@
 
 namespace App;
 
-use App\Filters\ProductFilters;
-use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Http\Request;
 
 class Product extends Model
 {
-
-    public function scopeFilter(Builder $builder, ProductFilters $filters)
-    {
-        $filters->apply($builder);
-    }
+    use HasFilters;
 }
